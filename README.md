11eaf325bb231403c78976050b0372f1447d437f
Zachary Pike <ZachTurnPike@gmail.com>
2021-05-31 01:21:22 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# EventEmitter implemtation in C++
+This is a header only library so just drag and drop the ``event-emitter`` directory into your include and ``#include "event-emitter/event-emitter.hpp"`` in your c++ program
+
+# Still in developemnt and not to be used seriously 
+You can use this if you want but i don't reccomend because i threw this together in 50 minutes
---
+++ event-emitter/eventemitter.hpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename args>
+struct CallBack {
+    function<void(args)> cb;
+
+    bool once;
+    int callTimes;
+
+};
+
+//EventEmitter Class
+template <typename T>
+class EventEmitter {
+    private:
+        unordered_map<string, vector<CallBack<T>>> callbacks;
+
+    public:
+        //When event is emmited on registered emitter, run the callback with the data emitted
+        void on(string evnt, function<void(T)> cb);
+
+        //Run callback only once after registered no matter how many times listener is emitted to
+        void once(string evnt, function<void(T)> cb);
+
+        //Emit event with data
+        void emit(string evnt, T data);
+};
+
+template <typename T>
+void EventEmitter<T>::on(string evnt, function<void(T)> cb) {
+    EventEmitter::callbacks[evnt].push_back((CallBack<T>) {
+        cb,
+        false,
+        0
+    });
+}
+
+
+template <typename T>
+void EventEmitter<T>::once(string evnt, function<void(T)> cb) {
+    EventEmitter::callbacks[evnt].push_back((CallBack<T>) {
+        cb,
+        true,
+        0
+    });
+}
+
+template <typename T>
+void EventEmitter<T>::emit(string evnt, T data) {
+    if (EventEmitter::callbacks.find(evnt) == EventEmitter::callbacks.end()) return;
+
+    //make a refrencee because im sick as shit of typing EventEmitter::callbacks, also used auto because im a lazy sack of shit
+    vector<CallBack<T>>& list = EventEmitter<T>::callbacks[evnt];
+
+    for (int i=0; i < list.size(); i++) {
+        if (list[i].callTimes > 0 && list[i].once) {
+            list.erase(list.begin() + i);
+        } else {
+            list[i].cb(data);
+            list[i].callTimes++;
+        }
+    }
+}
